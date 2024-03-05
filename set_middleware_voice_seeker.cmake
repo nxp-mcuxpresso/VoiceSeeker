cf0a3166b638937ae96e55bf9537bcf4dd095d37
mcu_sdk.ci <mcu_sdk.ci@nxp.com>
2024-03-05 14:22:25 +0000

    Add files for release/2.15.000_rw612_rfp3_gh_prep
    Signed-off-by: mcu_sdk.ci <mcu_sdk.ci@nxp.com>

--- set_middleware_voice_seeker.cmake
+++ set_middleware_voice_seeker.cmake
@@ -1,258 +1,60 @@
 include_guard(GLOBAL)
 
 
-if (CONFIG_USE_middleware_voice_seeker_cm4)
-# Add set(CONFIG_USE_middleware_voice_seeker_cm4 true) in config.cmake to use this component
+if (CONFIG_USE_middleware_voice_seeker_cm33_nodsp)
+# Add set(CONFIG_USE_middleware_voice_seeker_cm33_nodsp true) in config.cmake to use this component
 
-message("middleware_voice_seeker_cm4 component is included from ${CMAKE_CURRENT_LIST_FILE}.")
+message("middleware_voice_seeker_cm33_nodsp component is included from ${CMAKE_CURRENT_LIST_FILE}.")
 
-if((CONFIG_BOARD STREQUAL evkmimx8mm) AND CONFIG_USE_middleware_voice_seeker_rdsp_utilities_public_cm4)
-
-if(CONFIG_TOOLCHAIN STREQUAL armgcc)
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-  ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM4/include
-)
-endif()
-
-if(CONFIG_TOOLCHAIN STREQUAL armgcc)
-  target_link_libraries(${MCUX_SDK_PROJECT_NAME} PRIVATE
-    -Wl,--start-group
-      ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM4/lib/libvoiceseeker_no_aec.a
-      -Wl,--end-group
-  )
-endif()
-
-else()
-
-message(SEND_ERROR "middleware_voice_seeker_cm4 dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
-
-endif()
-
-endif()
-
-
-if (CONFIG_USE_middleware_voice_seeker_cm7)
-# Add set(CONFIG_USE_middleware_voice_seeker_cm7 true) in config.cmake to use this component
-
-message("middleware_voice_seeker_cm7 component is included from ${CMAKE_CURRENT_LIST_FILE}.")
-
-if((CONFIG_BOARD STREQUAL evkmimxrt1040 OR CONFIG_BOARD STREQUAL evkbimxrt1050 OR CONFIG_BOARD STREQUAL evkmimxrt1060 OR CONFIG_BOARD STREQUAL evkmimxrt1064 OR CONFIG_BOARD STREQUAL evkbmimxrt1060 OR CONFIG_BOARD STREQUAL evkcmimxrt1060 OR CONFIG_BOARD STREQUAL evkmimxrt1160 OR CONFIG_BOARD STREQUAL evkmimxrt1170 OR CONFIG_BOARD STREQUAL evkbmimxrt1170 OR CONFIG_BOARD STREQUAL evkmimx8mp) AND CONFIG_USE_middleware_voice_seeker_rdsp_utilities_public_cm7)
+if((CONFIG_BOARD STREQUAL rdrw612bga) AND CONFIG_USE_middleware_voice_seeker_rdsp_utilities_public_cm33_noDSP)
 
 if((CONFIG_TOOLCHAIN STREQUAL armgcc OR CONFIG_TOOLCHAIN STREQUAL mcux))
 target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-  ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM7/include
+  ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM33_noDSP/include
 )
 endif()
 
 if((CONFIG_TOOLCHAIN STREQUAL armgcc OR CONFIG_TOOLCHAIN STREQUAL mcux))
   target_link_libraries(${MCUX_SDK_PROJECT_NAME} PRIVATE
     -Wl,--start-group
-      ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM7/lib/libvoiceseeker_no_aec.a
+      ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM33_noDSP/lib/libvoiceseeker_no_aec.a
       -Wl,--end-group
   )
 endif()
 
 else()
 
-message(SEND_ERROR "middleware_voice_seeker_cm7 dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
+message(SEND_ERROR "middleware_voice_seeker_cm33_nodsp dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
 
 endif()
 
 endif()
 
 
-if (CONFIG_USE_middleware_voice_seeker_hifi4)
-# Add set(CONFIG_USE_middleware_voice_seeker_hifi4 true) in config.cmake to use this component
+if (CONFIG_USE_middleware_voice_seeker_rdsp_utilities_public_cm33_noDSP)
+# Add set(CONFIG_USE_middleware_voice_seeker_rdsp_utilities_public_cm33_noDSP true) in config.cmake to use this component
 
-message("middleware_voice_seeker_hifi4 component is included from ${CMAKE_CURRENT_LIST_FILE}.")
+message("middleware_voice_seeker_rdsp_utilities_public_cm33_noDSP component is included from ${CMAKE_CURRENT_LIST_FILE}.")
 
-if((CONFIG_BOARD STREQUAL evkmimxrt685 OR CONFIG_BOARD STREQUAL mimxrt685audevk) AND CONFIG_USE_middleware_voice_seeker_rdsp_utilities_public_hifi4)
-
-if(CONFIG_TOOLCHAIN STREQUAL xcc)
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-  ${CMAKE_CURRENT_LIST_DIR}/RT685_HiFi4/include
-)
-endif()
-
-if(CONFIG_USE_COMPONENT_CONFIGURATION)
-  message("===>Import configuration from ${CMAKE_CURRENT_LIST_FILE}")
-
-  target_compile_definitions(${MCUX_SDK_PROJECT_NAME} PUBLIC
-    -DHIFI4=1
-  )
-
-endif()
-
-if(CONFIG_TOOLCHAIN STREQUAL xcc)
-  target_link_libraries(${MCUX_SDK_PROJECT_NAME} PRIVATE
-    -Wl,--start-group
-      ${CMAKE_CURRENT_LIST_DIR}/RT685_HiFi4/lib/libvoiceseeker_no_aec.a
-      -Wl,--end-group
-  )
-endif()
-
-else()
-
-message(SEND_ERROR "middleware_voice_seeker_hifi4 dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
-
-endif()
-
-endif()
-
-
-if (CONFIG_USE_middleware_voice_seeker_fusionf1)
-# Add set(CONFIG_USE_middleware_voice_seeker_fusionf1 true) in config.cmake to use this component
-
-message("middleware_voice_seeker_fusionf1 component is included from ${CMAKE_CURRENT_LIST_FILE}.")
-
-if((CONFIG_BOARD STREQUAL evkmimxrt595) AND CONFIG_USE_middleware_voice_seeker_rdsp_utilities_public_fusionf1)
-
-if(CONFIG_TOOLCHAIN STREQUAL xcc)
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-  ${CMAKE_CURRENT_LIST_DIR}/RT595_FusionF1/include
-)
-endif()
-
-if(CONFIG_USE_COMPONENT_CONFIGURATION)
-  message("===>Import configuration from ${CMAKE_CURRENT_LIST_FILE}")
-
-  target_compile_definitions(${MCUX_SDK_PROJECT_NAME} PUBLIC
-    -DFUSIONDSP=1
-  )
-
-endif()
-
-if(CONFIG_TOOLCHAIN STREQUAL xcc)
-  target_link_libraries(${MCUX_SDK_PROJECT_NAME} PRIVATE
-    -Wl,--start-group
-      ${CMAKE_CURRENT_LIST_DIR}/RT595_FusionF1/lib/libvoiceseeker_no_aec.a
-      -Wl,--end-group
-  )
-endif()
-
-else()
-
-message(SEND_ERROR "middleware_voice_seeker_fusionf1 dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
-
-endif()
-
-endif()
-
-
-if (CONFIG_USE_middleware_voice_seeker_rdsp_utilities_public_cm4)
-# Add set(CONFIG_USE_middleware_voice_seeker_rdsp_utilities_public_cm4 true) in config.cmake to use this component
-
-message("middleware_voice_seeker_rdsp_utilities_public_cm4 component is included from ${CMAKE_CURRENT_LIST_FILE}.")
-
-if((CONFIG_CORE STREQUAL cm4f) AND (CONFIG_BOARD STREQUAL evkmimx8mm))
+if((CONFIG_CORE STREQUAL cm33) AND (CONFIG_BOARD STREQUAL rdrw612bga))
 
 if((CONFIG_TOOLCHAIN STREQUAL armgcc OR CONFIG_TOOLCHAIN STREQUAL mcux))
   target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
-      ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM4/rdsp_utilities_public/rdsp_memory_utils_public/RdspMemoryUtilsPublic.c
-      ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM4/rdsp_utilities_public/rdsp_memory_utils_public/memcheck.c
+      ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM33_noDSP/rdsp_utilities_public/rdsp_memory_utils_public/RdspMemoryUtilsPublic.c
+      ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM33_noDSP/rdsp_utilities_public/rdsp_memory_utils_public/memcheck.c
   )
 endif()
 
 if((CONFIG_TOOLCHAIN STREQUAL armgcc OR CONFIG_TOOLCHAIN STREQUAL mcux))
 target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-  ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM4/rdsp_utilities_public/include
-  ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM4/rdsp_utilities_public/rdsp_memory_utils_public
+  ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM33_noDSP/rdsp_utilities_public/include
+  ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM33_noDSP/rdsp_utilities_public/rdsp_memory_utils_public
 )
 endif()
 
 else()
 
-message(SEND_ERROR "middleware_voice_seeker_rdsp_utilities_public_cm4 dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
-
-endif()
-
-endif()
-
-
-if (CONFIG_USE_middleware_voice_seeker_rdsp_utilities_public_cm7)
-# Add set(CONFIG_USE_middleware_voice_seeker_rdsp_utilities_public_cm7 true) in config.cmake to use this component
-
-message("middleware_voice_seeker_rdsp_utilities_public_cm7 component is included from ${CMAKE_CURRENT_LIST_FILE}.")
-
-if((CONFIG_CORE STREQUAL cm7f) AND (CONFIG_BOARD STREQUAL evkmimxrt1040 OR CONFIG_BOARD STREQUAL evkbimxrt1050 OR CONFIG_BOARD STREQUAL evkmimxrt1060 OR CONFIG_BOARD STREQUAL evkmimxrt1064 OR CONFIG_BOARD STREQUAL evkbmimxrt1060 OR CONFIG_BOARD STREQUAL evkcmimxrt1060 OR CONFIG_BOARD STREQUAL evkmimxrt1160 OR CONFIG_BOARD STREQUAL evkmimxrt1170 OR CONFIG_BOARD STREQUAL evkbmimxrt1170 OR CONFIG_BOARD STREQUAL evkmimx8mp))
-
-if((CONFIG_TOOLCHAIN STREQUAL armgcc OR CONFIG_TOOLCHAIN STREQUAL mcux))
-  target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
-      ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM7/rdsp_utilities_public/rdsp_memory_utils_public/RdspMemoryUtilsPublic.c
-      ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM7/rdsp_utilities_public/rdsp_memory_utils_public/memcheck.c
-  )
-endif()
-
-if((CONFIG_TOOLCHAIN STREQUAL armgcc OR CONFIG_TOOLCHAIN STREQUAL mcux))
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-  ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM7/rdsp_utilities_public/include
-  ${CMAKE_CURRENT_LIST_DIR}/ARM_CortexM7/rdsp_utilities_public/rdsp_memory_utils_public
-)
-endif()
-
-else()
-
-message(SEND_ERROR "middleware_voice_seeker_rdsp_utilities_public_cm7 dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
-
-endif()
-
-endif()
-
-
-if (CONFIG_USE_middleware_voice_seeker_rdsp_utilities_public_hifi4)
-# Add set(CONFIG_USE_middleware_voice_seeker_rdsp_utilities_public_hifi4 true) in config.cmake to use this component
-
-message("middleware_voice_seeker_rdsp_utilities_public_hifi4 component is included from ${CMAKE_CURRENT_LIST_FILE}.")
-
-if((CONFIG_CORE STREQUAL dsp) AND (CONFIG_CORE_ID STREQUAL hifi4) AND (CONFIG_BOARD STREQUAL evkmimxrt685 OR CONFIG_BOARD STREQUAL mimxrt685audevk))
-
-if(CONFIG_TOOLCHAIN STREQUAL xcc)
-  target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
-      ${CMAKE_CURRENT_LIST_DIR}/RT685_HiFi4/rdsp_utilities_public/rdsp_memory_utils_public/RdspMemoryUtilsPublic.c
-      ${CMAKE_CURRENT_LIST_DIR}/RT685_HiFi4/rdsp_utilities_public/rdsp_memory_utils_public/memcheck.c
-  )
-endif()
-
-if(CONFIG_TOOLCHAIN STREQUAL xcc)
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-  ${CMAKE_CURRENT_LIST_DIR}/RT685_HiFi4/rdsp_utilities_public/include
-  ${CMAKE_CURRENT_LIST_DIR}/RT685_HiFi4/rdsp_utilities_public/rdsp_memory_utils_public
-)
-endif()
-
-else()
-
-message(SEND_ERROR "middleware_voice_seeker_rdsp_utilities_public_hifi4 dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
-
-endif()
-
-endif()
-
-
-if (CONFIG_USE_middleware_voice_seeker_rdsp_utilities_public_fusionf1)
-# Add set(CONFIG_USE_middleware_voice_seeker_rdsp_utilities_public_fusionf1 true) in config.cmake to use this component
-
-message("middleware_voice_seeker_rdsp_utilities_public_fusionf1 component is included from ${CMAKE_CURRENT_LIST_FILE}.")
-
-if((CONFIG_CORE STREQUAL dsp) AND (CONFIG_CORE_ID STREQUAL fusionf1) AND (CONFIG_BOARD STREQUAL evkmimxrt595))
-
-if(CONFIG_TOOLCHAIN STREQUAL xcc)
-  target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
-      ${CMAKE_CURRENT_LIST_DIR}/RT595_FusionF1/rdsp_utilities_public/rdsp_memory_utils_public/RdspMemoryUtilsPublic.c
-      ${CMAKE_CURRENT_LIST_DIR}/RT595_FusionF1/rdsp_utilities_public/rdsp_memory_utils_public/memcheck.c
-  )
-endif()
-
-if(CONFIG_TOOLCHAIN STREQUAL xcc)
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-  ${CMAKE_CURRENT_LIST_DIR}/RT595_FusionF1/rdsp_utilities_public/include
-  ${CMAKE_CURRENT_LIST_DIR}/RT595_FusionF1/rdsp_utilities_public/rdsp_memory_utils_public
-)
-endif()
-
-else()
-
-message(SEND_ERROR "middleware_voice_seeker_rdsp_utilities_public_fusionf1 dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
+message(SEND_ERROR "middleware_voice_seeker_rdsp_utilities_public_cm33_noDSP dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
 
 endif()
 
